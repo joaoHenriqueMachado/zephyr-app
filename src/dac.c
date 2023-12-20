01e6a11ebf894e34b41d55b5d99d093f3514fdc9
João Henrique <joaohenriquepereiramachado@gmail.com>
2023-12-20 14:33:28 -0300

    fix: dac initialization - �

--- src/dac.c
+++ src/dac.c
@@ -1,5 +1,508 @@
 #include "main.h"
 
+ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
+
+DAC_HandleTypeDef hdac;
+DMA_HandleTypeDef hdma_dac1;
+
+TIM_HandleTypeDef htim4;
+TIM_HandleTypeDef htim8;
+
+/* USER CODE BEGIN PV */
+struct k_sem adc_sem;
+uint16_t adcBuffer[256];
+float ReIm[256 * 2];
+float mod[256];
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 12;
+	RCC_OscInitStruct.PLL.PLLN = 64;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 4;
+
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+				      RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+}
+
+/**
+ * @brief ADC1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_ADC1_Init(void)
+{
+
+	/* USER CODE BEGIN ADC1_Init 0 */
+
+	/* USER CODE END ADC1_Init 0 */
+
+	ADC_ChannelConfTypeDef sConfig = {0};
+
+	/* USER CODE BEGIN ADC1_Init 1 */
+
+	/* USER CODE END ADC1_Init 1 */
+
+	/** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number
+	 * of conversion)
+	 */
+	hadc1.Instance = ADC1;
+	hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+	hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+	hadc1.Init.ScanConvMode = DISABLE;
+	hadc1.Init.ContinuousConvMode = DISABLE;
+	hadc1.Init.DiscontinuousConvMode = DISABLE;
+	hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
+	hadc1.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T8_TRGO;
+	hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	hadc1.Init.NbrOfConversion = 1;
+	hadc1.Init.DMAContinuousRequests = ENABLE;
+	hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+
+	/** Configure for the selected ADC regular channel its corresponding rank in the sequencer
+	 * and its sample time.
+	 */
+	sConfig.Channel = ADC_CHANNEL_1;
+	sConfig.Rank = 1;
+	sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+	/* USER CODE BEGIN ADC1_Init 2 */
+
+	/* USER CODE END ADC1_Init 2 */
+}
+
+/**
+ * @brief DAC Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_DAC_Init(void)
+{
+
+	/* USER CODE BEGIN DAC_Init 0 */
+
+	/* USER CODE END DAC_Init 0 */
+
+	DAC_ChannelConfTypeDef sConfig = {0};
+
+	/* USER CODE BEGIN DAC_Init 1 */
+
+	/* USER CODE END DAC_Init 1 */
+
+	/** DAC Initialization
+	 */
+	hdac.Instance = DAC;
+	if (HAL_DAC_Init(&hdac) != HAL_OK) {
+	}
+
+	/** DAC channel OUT1 config
+	 */
+	sConfig.DAC_Trigger = DAC_TRIGGER_T4_TRGO;
+	sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+	if (HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_1) != HAL_OK) {
+	}
+	/* USER CODE BEGIN DAC_Init 2 */
+
+	/* USER CODE END DAC_Init 2 */
+}
+
+/**
+ * @brief TIM4 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_TIM4_Init(void)
+{
+
+	/* USER CODE BEGIN TIM4_Init 0 */
+
+	/* USER CODE END TIM4_Init 0 */
+
+	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+	TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+	/* USER CODE BEGIN TIM4_Init 1 */
+
+	/* USER CODE END TIM4_Init 1 */
+	htim4.Instance = TIM4;
+	htim4.Init.Prescaler = 0;
+	htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim4.Init.Period = 11067;
+	htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+	if (HAL_TIM_Base_Init(&htim4) != HAL_OK) {
+	}
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK) {
+	}
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK) {
+	}
+	/* USER CODE BEGIN TIM4_Init 2 */
+
+	/* USER CODE END TIM4_Init 2 */
+}
+
+/**
+ * @brief TIM8 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_TIM8_Init(void)
+{
+
+	/* USER CODE BEGIN TIM8_Init 0 */
+
+	/* USER CODE END TIM8_Init 0 */
+
+	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+	TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+	/* USER CODE BEGIN TIM8_Init 1 */
+
+	/* USER CODE END TIM8_Init 1 */
+	htim8.Instance = TIM8;
+	htim8.Init.Prescaler = 0;
+	htim8.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim8.Init.Period = 20000;
+	htim8.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	htim8.Init.RepetitionCounter = 0;
+	htim8.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+	if (HAL_TIM_Base_Init(&htim8) != HAL_OK) {
+	}
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	if (HAL_TIM_ConfigClockSource(&htim8, &sClockSourceConfig) != HAL_OK) {
+	}
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	if (HAL_TIMEx_MasterConfigSynchronization(&htim8, &sMasterConfig) != HAL_OK) {
+	}
+	/* USER CODE BEGIN TIM8_Init 2 */
+
+	/* USER CODE END TIM8_Init 2 */
+}
+
+void DMA1_Stream5_IRQHandler(void)
+{
+	/* USER CODE BEGIN DMA1_Stream5_IRQn 0 */
+
+	/* USER CODE END DMA1_Stream5_IRQn 0 */
+	HAL_DMA_IRQHandler(&hdma_dac1);
+	/* USER CODE BEGIN DMA1_Stream5_IRQn 1 */
+
+	/* USER CODE END DMA1_Stream5_IRQn 1 */
+}
+
+/**
+ * @brief This function handles DMA2 stream0 global interrupt.
+ */
+void DMA2_Stream0_IRQHandler(void)
+{
+	/* USER CODE BEGIN DMA2_Stream0_IRQn 0 */
+
+	/* USER CODE END DMA2_Stream0_IRQn 0 */
+	HAL_DMA_IRQHandler(&hdma_adc1);
+	/* USER CODE BEGIN DMA2_Stream0_IRQn 1 */
+
+	/* USER CODE END DMA2_Stream0_IRQn 1 */
+}
+
+/**
+ * Enable DMA controller clock
+ */
+static void MX_DMA_Init(void)
+{
+
+	/* DMA controller clock enable */
+	__HAL_RCC_DMA2_CLK_ENABLE();
+	__HAL_RCC_DMA1_CLK_ENABLE();
+
+	IRQ_CONNECT(DMA1_Stream5_IRQn, 5, DMA1_Stream5_IRQHandler, 0, 0);
+	IRQ_CONNECT(DMA2_Stream0_IRQn, 5, DMA2_Stream0_IRQHandler, 0, 0);
+
+	/* DMA interrupt init */
+	/* DMA1_Stream5_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
+	/* DMA2_Stream0_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 0, 0);
+	HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void)
+{
+	/* USER CODE BEGIN MX_GPIO_Init_1 */
+	/* USER CODE END MX_GPIO_Init_1 */
+
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	/* USER CODE BEGIN MX_GPIO_Init_2 */
+	/* USER CODE END MX_GPIO_Init_2 */
+}
+
+void HAL_MspInit(void)
+{
+	/* USER CODE BEGIN MspInit 0 */
+
+	/* USER CODE END MspInit 0 */
+
+	__HAL_RCC_SYSCFG_CLK_ENABLE();
+	__HAL_RCC_PWR_CLK_ENABLE();
+
+	/* System interrupt init*/
+
+	/* USER CODE BEGIN MspInit 1 */
+
+	/* USER CODE END MspInit 1 */
+}
+
+/**
+ * @brief ADC MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param hadc: ADC handle pointer
+ * @retval None
+ */
+void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	if (hadc->Instance == ADC1) {
+		/* USER CODE BEGIN ADC1_MspInit 0 */
+
+		/* USER CODE END ADC1_MspInit 0 */
+		/* Peripheral clock enable */
+		__HAL_RCC_ADC1_CLK_ENABLE();
+
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+		/**ADC1 GPIO Configuration
+		PA1     ------> ADC1_IN1
+		*/
+		GPIO_InitStruct.Pin = GPIO_PIN_1;
+		GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+		/* ADC1 DMA Init */
+		/* ADC1 Init */
+		hdma_adc1.Instance = DMA2_Stream0;
+		hdma_adc1.Init.Channel = DMA_CHANNEL_0;
+		hdma_adc1.Init.Direction = DMA_PERIPH_TO_MEMORY;
+		hdma_adc1.Init.PeriphInc = DMA_PINC_DISABLE;
+		hdma_adc1.Init.MemInc = DMA_MINC_ENABLE;
+		hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+		hdma_adc1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+		hdma_adc1.Init.Mode = DMA_CIRCULAR;
+		hdma_adc1.Init.Priority = DMA_PRIORITY_LOW;
+		hdma_adc1.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+		if (HAL_DMA_Init(&hdma_adc1) != HAL_OK) {
+		}
+
+		__HAL_LINKDMA(hadc, DMA_Handle, hdma_adc1);
+
+		/* USER CODE BEGIN ADC1_MspInit 1 */
+
+		/* USER CODE END ADC1_MspInit 1 */
+	}
+}
+
+/**
+ * @brief ADC MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param hadc: ADC handle pointer
+ * @retval None
+ */
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc)
+{
+	if (hadc->Instance == ADC1) {
+		/* USER CODE BEGIN ADC1_MspDeInit 0 */
+
+		/* USER CODE END ADC1_MspDeInit 0 */
+		/* Peripheral clock disable */
+		__HAL_RCC_ADC1_CLK_DISABLE();
+
+		/**ADC1 GPIO Configuration
+		PA1     ------> ADC1_IN1
+		*/
+		HAL_GPIO_DeInit(GPIOA, GPIO_PIN_1);
+
+		/* ADC1 DMA DeInit */
+		HAL_DMA_DeInit(hadc->DMA_Handle);
+		/* USER CODE BEGIN ADC1_MspDeInit 1 */
+
+		/* USER CODE END ADC1_MspDeInit 1 */
+	}
+}
+
+/**
+ * @brief DAC MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param hdac: DAC handle pointer
+ * @retval None
+ */
+void HAL_DAC_MspInit(DAC_HandleTypeDef *hdac)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	if (hdac->Instance == DAC) {
+		/* USER CODE BEGIN DAC_MspInit 0 */
+
+		/* USER CODE END DAC_MspInit 0 */
+		/* Peripheral clock enable */
+		__HAL_RCC_DAC_CLK_ENABLE();
+
+		__HAL_RCC_GPIOA_CLK_ENABLE();
+		/**DAC GPIO Configuration
+		PA4     ------> DAC_OUT1
+		*/
+		GPIO_InitStruct.Pin = GPIO_PIN_4;
+		GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+		/* DAC DMA Init */
+		/* DAC1 Init */
+		hdma_dac1.Instance = DMA1_Stream5;
+		hdma_dac1.Init.Channel = DMA_CHANNEL_7;
+		hdma_dac1.Init.Direction = DMA_MEMORY_TO_PERIPH;
+		hdma_dac1.Init.PeriphInc = DMA_PINC_DISABLE;
+		hdma_dac1.Init.MemInc = DMA_MINC_ENABLE;
+		hdma_dac1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+		hdma_dac1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+		hdma_dac1.Init.Mode = DMA_CIRCULAR;
+		hdma_dac1.Init.Priority = DMA_PRIORITY_LOW;
+		hdma_dac1.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+		if (HAL_DMA_Init(&hdma_dac1) != HAL_OK) {
+		}
+
+		__HAL_LINKDMA(hdac, DMA_Handle1, hdma_dac1);
+
+		/* USER CODE BEGIN DAC_MspInit 1 */
+
+		/* USER CODE END DAC_MspInit 1 */
+	}
+}
+
+/**
+ * @brief DAC MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param hdac: DAC handle pointer
+ * @retval None
+ */
+void HAL_DAC_MspDeInit(DAC_HandleTypeDef *hdac)
+{
+	if (hdac->Instance == DAC) {
+		/* USER CODE BEGIN DAC_MspDeInit 0 */
+
+		/* USER CODE END DAC_MspDeInit 0 */
+		/* Peripheral clock disable */
+		__HAL_RCC_DAC_CLK_DISABLE();
+
+		/**DAC GPIO Configuration
+		PA4     ------> DAC_OUT1
+		*/
+		HAL_GPIO_DeInit(GPIOA, GPIO_PIN_4);
+
+		/* DAC DMA DeInit */
+		HAL_DMA_DeInit(hdac->DMA_Handle1);
+		/* USER CODE BEGIN DAC_MspDeInit 1 */
+
+		/* USER CODE END DAC_MspDeInit 1 */
+	}
+}
+
+/**
+ * @brief TIM_Base MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param htim_base: TIM_Base handle pointer
+ * @retval None
+ */
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
+{
+	if (htim_base->Instance == TIM4) {
+		/* USER CODE BEGIN TIM4_MspInit 0 */
+
+		/* USER CODE END TIM4_MspInit 0 */
+		/* Peripheral clock enable */
+		__HAL_RCC_TIM4_CLK_ENABLE();
+		/* USER CODE BEGIN TIM4_MspInit 1 */
+
+		/* USER CODE END TIM4_MspInit 1 */
+	} else if (htim_base->Instance == TIM8) {
+		/* USER CODE BEGIN TIM8_MspInit 0 */
+
+		/* USER CODE END TIM8_MspInit 0 */
+		/* Peripheral clock enable */
+		__HAL_RCC_TIM8_CLK_ENABLE();
+		/* USER CODE BEGIN TIM8_MspInit 1 */
+
+		/* USER CODE END TIM8_MspInit 1 */
+	}
+}
+
+/**
+ * @brief TIM_Base MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param htim_base: TIM_Base handle pointer
+ * @retval None
+ */
+void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *htim_base)
+{
+	if (htim_base->Instance == TIM4) {
+		/* USER CODE BEGIN TIM4_MspDeInit 0 */
+
+		/* USER CODE END TIM4_MspDeInit 0 */
+		/* Peripheral clock disable */
+		__HAL_RCC_TIM4_CLK_DISABLE();
+		/* USER CODE BEGIN TIM4_MspDeInit 1 */
+
+		/* USER CODE END TIM4_MspDeInit 1 */
+	} else if (htim_base->Instance == TIM8) {
+		/* USER CODE BEGIN TIM8_MspDeInit 0 */
+
+		/* USER CODE END TIM8_MspDeInit 0 */
+		/* Peripheral clock disable */
+		__HAL_RCC_TIM8_CLK_DISABLE();
+		/* USER CODE BEGIN TIM8_MspDeInit 1 */
+
+		/* USER CODE END TIM8_MspDeInit 1 */
+	}
+}
 uint16_t sin_wave[256] = {
 	2048, 2098, 2148, 2199, 2249, 2299, 2349, 2399, 2448, 2498, 2547, 2596, 2644, 2692, 2740,
 	2787, 2834, 2880, 2926, 2971, 3016, 3060, 3104, 3147, 3189, 3230, 3271, 3311, 3351, 3389,
@@ -40,16 +543,58 @@ uint16_t sin_wave_3rd_harmonic[256] = {
 	850,  915,  983,  1054, 1127, 1203, 1281, 1361, 1443, 1526, 1611, 1697, 1784, 1871, 1959,
 	2047};
 
+K_SEM_DEFINE(adc_sem, 0, 1);
+
+void adc_task()
+{
+	HAL_Init();
+	SystemClock_Config();
+	MX_GPIO_Init();
+	MX_DMA_Init();
+	MX_ADC1_Init();
+	MX_DAC_Init();
+	MX_TIM4_Init();
+	MX_TIM8_Init();
+
+	HAL_ADC_Start_DMA(&hadc1, (uint32_t *)adcBuffer, 256);
+	HAL_DAC_Start_DMA(&hdac, DAC_CHANNEL_1, (uint32_t *)sin_wave, 256, DAC_ALIGN_12B_R);
+
+	HAL_TIM_Base_Start(&htim8);
+	HAL_TIM_Base_Start(&htim4);
+
+	while (1) {
+		k_sem_take(&adc_sem, K_MSEC(50));
+		int k = 0;
+		for (int i = 0; i < 256; i++) {
+			ReIm[k] = (float)adcBuffer[i] * 0.0008056640625;
+			ReIm[k + 1] = 0.0;
+			k += 2;
+		}
+
+		arm_cfft_f32(&arm_cfft_sR_f32_len256, ReIm, 0, 1);
+		arm_cmplx_mag_f32(ReIm, mod, 256);
+		arm_scale_f32(mod, 0.0078125, mod, 128);
+
+		volatile float fund_phase =
+			atan2f(ReIm[3], ReIm[2]) * 180 / M_PI; // Fase R da harmonica fundamental
+		(void)fund_phase;
+
+		k_msleep(SLEEP_TIME_MS);
+	}
+}
+
+void adc0(void)
+{
+	adc_task();
+}
+
+K_THREAD_DEFINE(adc0_id, STACKSIZE, adc0, NULL, NULL, NULL, PRIORITY, 0, 0);
+
 static int generate_sine_cmd_handler(const struct shell *sh, size_t argc, char **argv, void *data)
 {
 	int signal;
 
 	signal = (int)data;
-
-	shell_print(sh,
-		    "Signal set to: %s\n"
-		    "Value sent to DAC driver: %d\n",
-		    argv[0], signal);
 
 	switch (signal) {
 	case SINE_SIGNAL:
@@ -65,6 +610,10 @@ static int generate_sine_cmd_handler(const struct shell *sh, size_t argc, char *
 		shell_print(sh, "Not a valid DAC signal\n");
 		break;
 	}
+	shell_print(sh,
+		    "Signal set to: %s\n"
+		    "Value sent to DAC driver: %d\n",
+		    argv[0], signal);
 	return 0;
 }
 
